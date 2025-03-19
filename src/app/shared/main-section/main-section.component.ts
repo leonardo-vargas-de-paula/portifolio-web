7dba614a45ea060acf3450ea72b5a357d773c5d8
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-03-19 19:37:56 -0400

    fix: botao cv import

--- src/app/shared/main-section/main-section.component.ts
+++ src/app/shared/main-section/main-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MainImageComponent } from "../main-image/main-image.component";
+import { ButtonPrimaryComponent } from "../button-primary/button-primary.component";
 
 @Component({
   selector: 'app-main-section',
-  imports: [MainImageComponent],
+  imports: [MainImageComponent, ButtonPrimaryComponent],
   templateUrl: './main-section.component.html',
   styleUrl: './main-section.component.css'
 })
